cd95fef19632e197f7bd10008bc93eb4ec12729f
Kevinrestrepoh <kevinrestrepoh@gmail.com>
2026-02-07 11:33:11 -0500

    vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { PortfolioShell } from './components/layout/PortfolioShell';
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
     <div className="relative min-h-screen overflow-hidden">
       <PortfolioShell />
+      <Analytics />
     </div>
   );
 }
